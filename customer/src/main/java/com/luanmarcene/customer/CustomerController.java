a5242a0b8eb235d692caaeda246e8ba91407eb10
Luan Marcene <luanmarcenemxz22@gmail.com>
2023-06-18 22:08:26 -0300

    ADDED: Created and configured docker-compose.yml

--- customer/src/main/java/com/luanmarcene/customer/CustomerController.java
+++ customer/src/main/java/com/luanmarcene/customer/CustomerController.java
@@ -10,13 +10,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customers")
-public record CustomerController() {
+public record CustomerController(CustomerService customerService) {
     
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
 
         log.info("new Customer Registration {}", customerRegistrationRequest);
-
+        customerService.registerCustomer(customerRegistrationRequest);
     }
 
 }
